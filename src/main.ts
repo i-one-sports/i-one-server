7ffe22c0e8d734ab7233cc6af8672fcea60b8bec
Samuel Theophilus <samueltheophilus019@gmail.com>
2025-06-03 16:32:10 +0100

    fix port issue

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,13 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
   const httpAdapter = app.get(HttpAdapterHost);
-
+  
   app.use(cookieParser());
-
+  
   app.useGlobalFilters(new GlobalExceptionFilter(httpAdapter));
   app.setGlobalPrefix('api/v1');
-  await app.listen(3000);
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
